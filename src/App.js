4f3f5d425d31965fde91f9ba3cfeb8380c0af1bb
Brandon Collins <brandoncollins@Brandons-iMac.local>
2021-01-14 11:11:04 -0500

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      {/* // Switch: the moment a route is found, it will only render that route */}
+      <Switch>
+        {/* // without exact, as long as the path matches then the component will
+        render */}
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
